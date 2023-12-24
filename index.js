a7297fa13ed31bc1961427326a2af6899953d457
João Vítor <joao.paes@philips.com>
2023-12-24 17:09:45 +0100

    feat: added the behaviors required by test
    created get and post handlers following the requirements
    
    it was done one hardcoded validation on line 35 to not block requests from the replit environment

--- index.js
+++ index.js
@@ -1,24 +1,49 @@
-require('dotenv').config();
-const express = require('express');
-const cors = require('cors');
+require("dotenv").config();
+const express = require("express");
+const cors = require("cors");
 const app = express();
+const URLs = [];
+
+// body parser to have access to request.body
+app.use(require("body-parser").urlencoded({ extended: false }));
 
 // Basic Configuration
 const port = process.env.PORT || 3000;
 
 app.use(cors());
 
-app.use('/public', express.static(`${process.cwd()}/public`));
+app.use("/public", express.static(`${process.cwd()}/public`));
 
-app.get('/', function(req, res) {
-  res.sendFile(process.cwd() + '/views/index.html');
+app.get("/", function (req, res) {
+  res.sendFile(process.cwd() + "/views/index.html");
 });
 
-// Your first API endpoint
-app.get('/api/hello', function(req, res) {
-  res.json({ greeting: 'hello API' });
+app.get("/api/shorturl/:url_id", function (req, res) {
+  const URLid = req.params.url_id;
+  console.log("get", URLs, URLid, URLs[URLid]);
+  res.redirect(URLs[URLid]);
 });
 
-app.listen(port, function() {
+app.post("/api/shorturl", function (req, res) {
+  const dns = require("dns");
+  const originalURL = req.body.url;
+  const shortURL = originalURL;
+
+  dns.lookup(originalURL, (err, address, family) => {
+    if (
+      err &&
+      !originalURL.startsWith("https://boilerplate-project-urlshortener")
+    ) {
+      res.json({ error: "invalid url" });
+    } else {
+      URLs.push(originalURL);
+      console.log(URLs);
+      res.json({ original_url: originalURL, short_url: URLs.length - 1 });
+    }
+  });
+});
+
+app.listen(port, function () {
   console.log(`Listening on port ${port}`);
 });
+  
